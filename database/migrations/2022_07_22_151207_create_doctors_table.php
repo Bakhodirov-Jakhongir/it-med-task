03d845f071021eb4ca847f41698570110575e00b
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-07-22 21:17:47 +0500

    create migrations:patients,doctors,institutions and appointments

---
+++ database/migrations/2022_07_22_151207_create_doctors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Enums\DoctorType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->enum('type', DoctorType::getValues())->default(DoctorType::GENERAL_DOCTOR);
+            $table->unsignedTinyInteger('experience');
+            $table->string('phone_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
